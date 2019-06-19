591eb8d60ed07d9f163fe0e4a162e8d24c49e39a
Syphdias <syphdias+git@gmail.com>
2019-06-19 16:17:45 +0200

    optional virtenvwrapper

--- .zshrc.d/virtenvwrapper.zsh
+++ .zshrc.d/virtenvwrapper.zsh
@@ -1,4 +1,6 @@
-export WORKON_HOME="${HOME}/.virtualenvs"
-export PROJECT_HOME="${HOME}/ves"
-export VIRTUALENVWRAPPER_PYTHON="/usr/bin/python3"
-source ~/.local/bin/virtualenvwrapper.sh
+if [[ -f ~/.local/bin/virtualenvwrapper.sh ]]; then
+    export WORKON_HOME="${HOME}/.virtualenvs"
+    export PROJECT_HOME="${HOME}/ves"
+    export VIRTUALENVWRAPPER_PYTHON="/usr/bin/python3"
+    source ~/.local/bin/virtualenvwrapper.sh
+fi
