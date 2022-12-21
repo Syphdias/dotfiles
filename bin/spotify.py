f75baa7f5aa288947c2bf6c081ce7e0747e85d4a
Syphdias <syphdias+git@gmail.com>
2022-12-21 17:39:48 +0100

    Simplify i3block spotify script

--- bin/spotify.py
+++ bin/spotify.py
@@ -1,38 +1,42 @@
-#!/usr/bin/env python3
-
-import dbus
-import os
-import sys
+#!/usr/bin/env python
+from dbus import SessionBus, Interface
+from dbus.exceptions import DBusException
+from os import environ
+from sys import version_info
 import subprocess
 
-
+spotify = None
 try:
-    bus = dbus.SessionBus()
-    spotify = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+    bus = SessionBus()
+    spotify = bus.get_object(
+        "org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2"
+    )
+except DBusException:
+    exit()
 
-    if os.environ.get('BLOCK_BUTTON'):
-        control_iface = dbus.Interface(spotify, 'org.mpris.MediaPlayer2.Player')
-        if (os.environ['BLOCK_BUTTON'] == '1'):
-            control_iface.Previous()
-        elif (os.environ['BLOCK_BUTTON'] == '2'):
-            control_iface.PlayPause()
-        elif (os.environ['BLOCK_BUTTON'] == '3'):
-            control_iface.Next()
-        elif (os.environ['BLOCK_BUTTON'] == '3'):
-            control_iface.Next()
-        elif (os.environ['BLOCK_BUTTON'] == '4'):
-            subprocess.call(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%"])
-        elif (os.environ['BLOCK_BUTTON'] == '5'):
-            subprocess.call(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%"])
+# Handle clicks to the i3block
+if block_button := environ.get("BLOCK_BUTTON"):
+    control_iface = Interface(spotify, "org.mpris.MediaPlayer2.Player")
+    if block_button == "1":
+        control_iface.Previous()
 
-    spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
-    props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+    elif block_button == "2":
+        control_iface.PlayPause()
 
-    if (sys.version_info > (3, 0)):
-        print(str(props['xesam:artist'][0]) + " - " + str(props['xesam:title']))
-    else:
-        print("<span color='#2ebd59'></span>" + props['xesam:artist'][0] + " - " + props['xesam:title'])
-        # removed _after_ print .encode('utf-8')
-    exit
-except dbus.exceptions.DBusException:
-    exit
+    elif block_button == "3":
+        control_iface.Next()
+
+    elif block_button == "3":
+        control_iface.Next()
+
+    elif block_button == "4":
+        subprocess.call(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%"])
+
+    elif block_button == "5":
+        subprocess.call(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%"])
+
+# Get information from spotify object
+spotify_iface = Interface(spotify, "org.freedesktop.DBus.Properties")
+props = spotify_iface.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+
+print(f'{props["xesam:artist"][0]} – {props["xesam:title"]}')
