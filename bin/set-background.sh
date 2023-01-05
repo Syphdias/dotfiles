521cc56a27f991995e1585ecd0f254717f6cac6a
Syphdias <syphdias+git@gmail.com>
2023-01-05 17:22:58 +0100

    Fix background/wallpaper automation

--- bin/set-background.sh
+++ bin/set-background.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-source "$(dirname $0)/postswitchtheme.src"
+source ~/.config/dynamic-wallpaper-theme.src
 feh --bg-fill ~/.local/share/dynamic-wallpaper/${POSTSWITCHTHEME}/$(($(date +\%k)/1)).*
 #feh --bg-fill ~/Pictures/wallpapers/current/ --randomize
