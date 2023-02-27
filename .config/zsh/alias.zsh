0adbeca299c08a63174cd4ea579ee12f0c3e47f9
Syphdias <syphdias+git@gmail.com>
2023-02-27 22:57:04 +0100

    Clean out a few unused aliases

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -6,9 +6,6 @@ alias dotprivate='/usr/bin/git --git-dir="${XDG_CONFIG_HOME:-${HOME}/.config}/do
 alias dotcheat='/usr/bin/git --git-dir="${XDG_CONFIG_HOME:-${HOME}/.config}/dotgit/cheat/" --work-tree="${HOME}"'
 
 # aliases
-## ssh aliases
-alias ssr='ssh -lroot'
-alias ssa='ssh -ladmin'
 ## git aliases
 alias gst='git status'
 alias gr='git pull --rebase'
@@ -29,8 +26,7 @@ alias s='screen -rd ddd'
 alias t='timew'
 alias c=' noglob calc'
 alias tree='tree -a -I .git'
-alias doc='docker-compose'
-#alias s3cmd='s3cmd --signature-v2'
+alias doc='docker compose'
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
@@ -40,15 +36,7 @@ alias dict=' dict'
 alias vless='vim -u ~/.config/vim/vimrc.less -'
 alias apachelog='sudo tail -f /var/log/apache2/error.log'
 alias ds='dig +short'
-alias pcd='for ((i=$((60-$(date +%S))); i>0; i--)); do clear; figlet "$i" |lolcat -F 0.5; sleep 1; done'
-alias pcde='pcd; exit'
-alias mcde='mcd; exit'
-alias fixmonworkleft='xrandr --output eDP-1 --mode 1360x768 --pos 0x0 --output DP-2-1 --mode 1920x1200 --pos 1360x-432 --primary --output DP-2-2 --mode 1920x1200 --pos 3280x-432'
-alias fixmonworkright='xrandr --output eDP-1 --mode 1360x768 --pos 3840x432 --output DP-2-1 --mode 1920x1200 --pos 1920x0 --primary --output DP-2-2 --mode 1920x1200 --pos 0x0'
-#alias atom='atom-beta .'
-alias winboot='win_id=$(sudo grep -ie "^menuentry '\''" -e "^submenu '\''" /boot/grub/grub.cfg --color |awk -v nr=99999 '\''/Windows/ && NR < nr {nr=NR; print NR-1}'\'') ; sudo grub-reboot $win_id'
 alias zshrc="vim ~/.zshrc"
-alias vim='nvim'
 alias aecho=' echo; echo'
 alias k='kubectl'
 
