88e27bde743aac49fb4306985b54ce09ec257fb3
Christian Kugler <syphdias+git@gmail.com>
2023-11-18 09:07:04 +0100

    Work around which-key bug

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,23 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  opts = {
-    current_line_blame = true,
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+    },
+  },
+
+  -- workaround for https://github.com/LazyVim/LazyVim/issues/1955
+  {
+    dir = "/home/syphdias/git/private/which-key.nvim",
+    opts = {
+      defaults = {
+        mode = { "n", "v" },
+        -- group = true,
+        ["<leader>gh"] = {
+          name = "+hunks",
+          [" "] = { "" },
+        },
+      },
+    },
   },
 }
