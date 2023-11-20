e5293f80b3788225340a153c96a221253fc3dfc6
Christian Kugler <syphdias+git@gmail.com>
2023-11-21 00:26:40 +0100

    Disable neovim LazyVim dashboard-nvim

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    -- not needed; alternatives:
+    -- restore(<leader>qs or <leader>ql), recent(<leader>fr), find (<leader><space>)
+    "nvimdev/dashboard-nvim",
+    enabled = false,
+  },
+}
