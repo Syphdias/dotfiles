3884ada0a883103013d866da5e7b20bf3adecf47
Christian Kugler <syphdias+git@gmail.com>
2023-11-22 09:53:45 +0100

    Remove old colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "bluz71/vim-nightfly-guicolors",
-  -- make sure to load this before all the other plugins
-  priority = 1000,
-  config = function()
-    -- load the colorscheme
-    vim.cmd([[colorscheme nightfly]])
-  end,
-}
