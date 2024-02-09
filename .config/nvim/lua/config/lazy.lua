b76345f0d2ce5f59bd58c19e3382a9492741be67
Christian Kugler <syphdias+git@gmail.com>
2024-02-09 01:13:41 +0100

    Lower checker interval for lazy

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -35,7 +35,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "catppuccin" } },
-  checker = { enabled = true, frequency = 86400 }, -- automatically check for plugin updates
+  checker = { enabled = true, frequency = 604800 }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
