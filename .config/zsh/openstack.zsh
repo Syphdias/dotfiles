52eff1f03f9da0440c795b89e3f7bda9a4c073c2
Christian Kugler <syphdias+git@gmail.com>
2024-05-27 19:20:11 +0200

    Add _ON_COMMAND for openstack

--- .config/zsh/openstack.zsh
+++ .config/zsh/openstack.zsh
@@ -4,3 +4,5 @@ prompt_openstack() {
     [[ -n "$OS_CLOUD" ]] \
         && p10k segment -f 197 -t $OS_CLOUD
 }
+
+typeset -g POWERLEVEL9K_OPENSTACK_SHOW_ON_COMMAND='barbican|ceilometer|cinder|cloudkitty|freezer|glance|heat|keystone|magnum|manila|mistral|monasca|murano|neutron|nova|openstack|swift|tacker|trove|vitrage|watcher'
