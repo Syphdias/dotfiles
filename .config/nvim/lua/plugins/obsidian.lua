d34442d2f094aca287a3ea33b93da72c0bddfabf
Christian Kugler <syphdias+git@gmail.com>
2024-09-29 15:54:19 +0200

    Improve obsidian.nvim

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,17 +1,56 @@
 return {
-  "epwalsh/obsidian.nvim",
-  lazy = true,
-  event = {
-    "BufReadPre /home/syphdias/Documents/obsidian-palace/**.md",
-    "BufNewFile /home/syphdias/Documents/obsidian-palace/**.md",
+  {
+    "epwalsh/obsidian.nvim",
+    lazy = true,
+    event = {
+      "BufReadPre /home/syphdias/Documents/obsidian-palace/**.md",
+      "BufNewFile /home/syphdias/Documents/obsidian-palace/**.md",
+    },
+    cmd = {
+      "ObsidianNew",
+      "ObsidianOpen",
+      "ObsidianQuickSwitch",
+      "ObsidianFollowLink",
+      "ObsidianBacklinks",
+      "ObsidianTags",
+      "ObsidianToday",
+      "ObsidianYesterday",
+      "ObsidianTomorrow",
+      "ObsidianDailies",
+      "ObsidianTemplate",
+      "ObsidianSearch",
+      "ObsidianLink",
+      "ObsidianLinkNew",
+      "ObsidianLinks",
+      "ObsidianExtractNote",
+      "ObsidianWorkspace",
+      "ObsidianPasteImg",
+      "ObsidianRename",
+      "ObsidianToggleCheckbox",
+      "ObsidianNewFromTemplate",
+      "ObsidianTOC",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      disable_frontmatter = true,
+      dir = "~/Documents/obsidian-palace",
+    },
   },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
+
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>o", group = "Obsidian" },
+        { "<leader>oo", "<cmd>ObsidianQuickSwitch<cr>", desc = "ObsidianQuickSwitch" },
+        { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "ObsidianBacklinks" },
+        { "<leader>ot", "<cmd>ObsidianToday<cr>", desc = "ObsidianToday" },
+        { "<leader>oy", "<cmd>ObsidianYesterday<cr>", desc = "ObsidianYesterday" },
+        { "<c-cr>", "<cmd>ObsidianToggleCheckbox<cr>", desc = "ObsidianToggleCheckbox" },
+        { "[[", "<cmd>ObsidianLink<cr>", desc = "ObsidianLink" },
+      },
+    },
   },
-  opts = {
-    disable_frontmatter = true,
-    dir = "/home/syphdias/Documents/obsidian-palace",
-  },
-  -- TODO: gf cannot be rebound because it is set elsewhere
-  -- https://github.com/epwalsh/obsidian.nvim/issues/162
 }
