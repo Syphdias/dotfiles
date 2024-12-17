4b0335f14a29be481f5201c537705fcb270fe656
Christian Kugler <syphdias+git@gmail.com>
2024-12-17 22:02:11 +0100

    Switch configuration to blink.cmp

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "Saghen/blink.cmp",
+    opts = function(_, opts)
+      opts.completion.ghost_text.enabled = false
+      opts.keymap = vim.tbl_extend("force", opts.keymap, {
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-Space>"] = { "select_and_accept", "fallback" },
+      })
+    end,
+  },
+}
