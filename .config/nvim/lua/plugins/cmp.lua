08de4de3a2ff8ae2aefc5fd0308c8deddb8cba1e
Christian Kugler <syphdias+git@gmail.com>
2024-12-19 23:31:34 +0100

    Finally fix blink.cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,16 +1,19 @@
 return {
   {
-    "Saghen/blink.cmp",
+    "saghen/blink.cmp",
     opts = function(_, opts)
+      -- do not preview for completion
       opts.completion.ghost_text.enabled = false
-      -- opts.keymap = vim.tbl_extend("keep", opts.keymap, {
-      opts.keymap = {
+
+      -- casing is important for keymap merge. The preset uses <C-space>; if we
+      -- use <C-Space> here, we sometimes get one behavior and sometimes the
+      -- other (this stole an hour of my life)
+      opts.keymap = vim.tbl_extend("force", opts.keymap, {
         ["<C-j>"] = { "select_next", "fallback" },
         ["<C-k>"] = { "select_prev", "fallback" },
-        ["<C-Space>"] = { "select_and_accept", "fallback" },
+        ["<C-space>"] = { "select_and_accept", "show", "fallback" },
         ["<CR>"] = {},
-      }
-      -- })
+      })
     end,
   },
 }
