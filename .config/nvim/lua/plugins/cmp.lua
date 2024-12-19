e2636149c8ef90eb6f6d12fda9bb5f4383e0a987
Christian Kugler <syphdias+git@gmail.com>
2024-12-19 23:20:49 +0100

    blink.cmp behaves weird

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,12 +3,14 @@ return {
     "Saghen/blink.cmp",
     opts = function(_, opts)
       opts.completion.ghost_text.enabled = false
-      opts.keymap = vim.tbl_extend("keep", opts.keymap, {
+      -- opts.keymap = vim.tbl_extend("keep", opts.keymap, {
+      opts.keymap = {
         ["<C-j>"] = { "select_next", "fallback" },
         ["<C-k>"] = { "select_prev", "fallback" },
         ["<C-Space>"] = { "select_and_accept", "fallback" },
         ["<CR>"] = {},
-      })
+      }
+      -- })
     end,
   },
 }
