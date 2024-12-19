d4eddd29c7d927fcc55460b6cac55c5c0c322692
Christian Kugler <syphdias+git@gmail.com>
2024-12-19 23:00:18 +0100

    Fix completion bindings

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,10 +3,11 @@ return {
     "Saghen/blink.cmp",
     opts = function(_, opts)
       opts.completion.ghost_text.enabled = false
-      opts.keymap = vim.tbl_extend("force", opts.keymap, {
+      opts.keymap = vim.tbl_extend("keep", opts.keymap, {
         ["<C-j>"] = { "select_next", "fallback" },
         ["<C-k>"] = { "select_prev", "fallback" },
-        ["<C-Space>"] = { "accept", "fallback" },
+        ["<C-Space>"] = { "select_and_accept", "fallback" },
+        ["<CR>"] = {},
       })
     end,
   },
