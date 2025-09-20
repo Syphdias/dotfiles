5498fed9a832aa2e3602f5938412fc3d52900aeb
Christian Kugler <syphdias+git@gmail.com>
2025-09-20 16:44:15 +0200

    Snack some images

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,8 +1,11 @@
 return {
   "folke/snacks.nvim",
+  -- dir = "/home/syphdias/git/private/snacks.nvim",
+  ---@type snacks.Config
   opts = {
     dashboard = { enabled = false },
     indent = { animate = { enabled = false } },
     scroll = { enabled = false },
+    image = {},
   },
 }
