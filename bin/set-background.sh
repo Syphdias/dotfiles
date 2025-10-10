c06ae07c26e12eda4987262869186649e28891c2
Christian Kugler <syphdias+git@gmail.com>
2025-10-10 13:34:07 +0200

    Fix set-background.sh
    - swaybg for sway
    - only switch if no active process (not tested for `feh`)

--- bin/set-background.sh
+++ bin/set-background.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 source ~/.config/dynamic-wallpaper-theme.src
-# feh --bg-fill ~/.local/share/dynamic-wallpaper/${POSTSWITCHTHEME}/$(($(date +\%k) / 1)).*
-feh --bg-fill ~/Pictures/wallpapers/current --randomize
+
+if [[ -n "$POSTSWITCHTHEME" ]]; then
+    new_bg=~/.local/share/dynamic-wallpaper/${POSTSWITCHTHEME}/$(($(date +\%k) / 1)).*
+else
+    new_bg=~/Pictures/wallpapers/current
+fi
+
+if pgrep -f "$new_bg" >/dev/null; then
+    echo "No bg change"
+    exit
+fi
+
+if [[ "$DESKTOP_SESSION" == sway ]]; then
+    swaymsg output '*' bg "$new_bg" fill
+else
+    feh --bg-fill "$new_bg" --randomize
+fi
