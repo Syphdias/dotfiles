faa671757fd42be9856a64246af7cb256afc181f
Christian Kugler <syphdias+git@gmail.com>
2026-03-27 23:26:04 +0100

    Update formatters for nvim

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,7 +2,9 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      nix = { "alejandra" },
+      ["nix"] = { "alejandra" },
+      ["markdown"] = { "markdownlint-cli2", "markdown-toc" },
+      ["markdown.mdx"] = { "markdownlint-cli2", "markdown-toc" },
     },
   },
 }
